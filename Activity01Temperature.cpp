fabad8836831acf978702bbc3d68887dfbc569a0
Nomarie S. Garido <nomariexgarido@gmail.com>
2024-11-20 08:25:03 +0800

    Add files via upload

---
+++ Activity01Temperature.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+	int temperature;
+	cout << "Enter the temperature in degree:";
+	cin >> temperature;
+	if(temperature <32) {
+		cout << "Bring a heavy jacket";
+	}
+	else if(temperature >= 32 && temperature<= 50) {
+		cout << "Bring a light jacket";
+	}
+	else if(temperature > 50) {
+		cout << "Not bring any jacket";
+	}
+	return 0;
+}
